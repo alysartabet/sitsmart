fee82fd05bbf6ffac722b115341d0e461092d21e
sameer <ramsam9257@gmail.com>
2025-03-31 13:47:56 -0400

    Started building tables

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,11 +1,52 @@
 # Represents the tables being added to the database
 from . import db
+from uuid import uuid4
+from sqlalchemy.dialects.postgresql import UUID
 
-# class User = table User
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
+# Buildings Table - building_id (primary key), building_name
+class Buildings(db.Model):
+    __tablename__ = 'building'
+
+    building_id = db.Column(db.String(4), primary_key=True, nullable=False)
+    building_name = db.Column(db.String(50), unique=True, nullable=False)
 
     def __repr__(self):
-        return f'<User {self.username}>'
+        return f'<Building {self.building_id}>'
+    
+# Rooms Table - room_id (primary key), room_num, building_id (foreign key), room_type, room_capacity, num_computers, num_whiteboards, rating
+class Rooms(db.Model):
+    __tablename__ = 'room'
+
+    room_id = db.Column(db.String(10), primary_key=True, nullable=False)
+    room_num = db.Column(db.Integer, nullable=False)
+    building_id = db.Column(db.String(4), db.ForeignKey('building.building_id'), nullable=False)
+    room_type = db.Column(db.String(15))
+    room_capacity = db.Column(db.Integer, default=0)
+    num_computers = db.Column(db.Integer, default=0)
+    num_whiteboards = db.Column(db.Integer, default=0)
+    rating = db.Column(db.Float, default=0.0)
+
+    def __repr__(self):
+        return f'<Room {self.room_id}>'
+
+# Room Rate - room_id (foreign key), event_id, event_rate, noise_rate, equipment_rate
+
+# Room Event - event_id (primary key), room_id (foreign key), event_name, event_organizer, organizer_id, is_faculty)
+class RoomEvent(db.Model):
+    __tablename__ = 'room_event'
+
+    event_id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid4, nullable=False)
+    room_id = db.Column(db.String(10), db.ForeignKey('room.room_id'), nullable=False)
+    event_name = db.Column(db.String(50), nullable=False)
+    event_organizer = db.Column(db.String(75), nullable=False)
+    organizer_id = db.Column(db.String(75), nullable=False)
+    is_faculty = db.Column(db.Boolean, default=False)
+
+    def __repr__(self):
+        return f'<RoomEvent {self.event_id}>'
+
+# Room Availability
+# Department
+# Course
+# Faculty
+# Student
--- backend/main.py
+++ backend/main.py
@@ -5,5 +5,9 @@ from flask_migrate import Migrate
 app = create_app()
 migrate = Migrate(app, db)
 
+# Create the database and tables if they don't exist (only done when app context pushed)
+with app.app_context():
+    db.create_all()
+
 if __name__ == '__main__':
     app.run(debug=True)
