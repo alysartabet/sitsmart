4b60c9742845ba80a813886d135a38129b359b88
sameer <ramsam9257@gmail.com>
2025-04-14 11:05:42 -0400

    fixing connection to routes.py

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -1,9 +1,18 @@
 # Displays the API routes where you can view all the tables in the DB
-from flask import Blueprint, jsonify, request
+from flask import Blueprint
 from . import db
-from .models import Buildings
+from .models import *
+from dotenv import load_dotenv
+from urllib.parse import urlparse
+import psycopg2
+import os
 
+load_dotenv()
 main = Blueprint('main', __name__)
+
+# Function to connect to database
+# def db_conn():
+#     conn = psycopg2.connect(urlPa
 
 # Using API calls for CRUD operations
 @main.route('/')
