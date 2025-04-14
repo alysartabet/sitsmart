e4dbf646b2c0f3173dd83ad756515cd9cc4f4869
sameer <ramsam9257@gmail.com>
2025-04-14 12:14:30 -0400

    Created connection function in routes.py

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -10,11 +10,26 @@ import os
 load_dotenv()
 main = Blueprint('main', __name__)
 
-# Function to connect to database
-# def db_conn():
-#     conn = psycopg2.connect(urlPa
+# Function to connect to database -> returns the PSQL connection object
+def db_conn():
+    """Grabs the connection string from the environment variable and returns a connection object."""
+    connection_string = os.getenv("DATABASE_URL")
+    p = urlparse(connection_string)
+    pg_connection_dict = {
+        "host": p.hostname,
+        "port": p.port,
+        "user": p.username,
+        "password": p.password,
+        "database": p.path[1:],
+    }
+    conn = psycopg2.connect(**pg_connection_dict)
+    return conn
+
 
 # Using API calls for CRUD operations
 @main.route('/')
 def index():
-    return "Welcome to the API!"
+    """Main route/home screen for the Sit Smart App"""
+    return "Welcome to the Smart Room API!"
+
+
